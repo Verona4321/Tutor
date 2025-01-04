ac3784e24329ba009ad055e12e7d1b111395ab24
WeronikaGRIG <veronikaudot@gmail.com>
2025-01-04 14:36:53 +0100

    refactor: changes font-size, edit main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,20 @@
-let swiper1 = new Swiper(".swiper-slider-1", {
+let swiper1 = new Swiper(".swiper-slider", {
   centeredSlides: true,
-  slidesPerView: 1,
+  slidesPerView: 2,
   grabCursor: true,
   freeMode: false,
   loop: true,
   mousewheel: false,
-  direction: 'ltr',
   keyboard: {
     enabled: true
   },
 
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false
-  },
-
   breakpoints: {
     640: {
-      slidesPerView: 1.25,
-
+      slidesPerView: 1.25
     },
     1024: {
-      slidesPerView: 2,
-
-    }
-  }
-});
-
-let swiper2 = new Swiper(".swiper-slider-2", {
-  centeredSlides: true,
-  slidesPerView: 1,
-  grabCursor: true,
-  freeMode: false,
-  loop: true,
-  mousewheel: false,
-  direction: 'rtl',
-  keyboard: {
-    enabled: true
-  },
-  autoplay: {
-    delay: 1000,
-    disableOnInteraction: false
-  },
-
-  breakpoints: {
-    640: {
-      slidesPerView: 1.25,
-    },
-    1024: {
-      slidesPerView: 2,
+      slidesPerView: 2
     }
   }
 });
